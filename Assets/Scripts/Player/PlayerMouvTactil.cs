9f98f283d587d374d3c1b13013a9ffaa1a5a1d16
sorlinV <valentin.sorlin@hotmail.com>
2018-01-08 21:40:55 +0100

    tactil pixel perfect behind finger

--- Assets/Scripts/Player/PlayerMouvTactil.cs
+++ Assets/Scripts/Player/PlayerMouvTactil.cs
@@ -6,7 +6,7 @@ public class PlayerMouvTactil : MonoBehaviour
 {
     Vector3 BlockOnCamera(Vector3 pos)
     {
-        float dist = (pos - Camera.main.transform.position).z;
+        float dist = Mathf.Abs(pos.z - Camera.main.transform.position.z);
 
         float leftBorder = Camera.main.ViewportToWorldPoint(
             new Vector3(0, 0, dist)).x;
@@ -20,28 +20,43 @@ public class PlayerMouvTactil : MonoBehaviour
         float bottomBorder = Camera.main.ViewportToWorldPoint(
             new Vector3(0, 1, dist)).y;
 
+        Debug.Log(dist);
+
+        float width = Mathf.Abs(rightBorder);
+        float heigth = Mathf.Abs(bottomBorder);
+        float ratio_x = width / Camera.main.pixelWidth;
+        float ratio_y = heigth / Camera.main.pixelHeight;
+        //Debug.Log("player world" + width + "; " + heigth);
+        //Debug.Log("screen" + Screen.width + "; " + Screen.height);
+        //Debug.Log("ratio" + ratio_x + "; " + ratio_y);
+        //Debug.Log(pos);
+        pos = transform.position + new Vector3(pos.x * ratio_x, pos.y * ratio_y, pos.z);
         pos = new Vector3(
             Mathf.Clamp(pos.x, leftBorder, rightBorder),
             Mathf.Clamp(pos.y, topBorder, bottomBorder),
             pos.z);
+        Debug.Log(pos);
         return (pos);
     }
 
-    void Update()
+    Vector2 mousePos = new Vector2(-1, -1);
+    void FixedUpdate()
     {
-        Camera c = Camera.main;
+        if (mousePos.x == -1 && mousePos.y == -1)
+        {
+            mousePos = new Vector2(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2);
+        }
         Vector3 p = new Vector3();
-        Event e = Event.current;
-        float dist = (transform.position - Camera.main.transform.position).z;
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
-            Vector2 mousePos = Input.GetTouch(0).deltaPosition;
-            p = c.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, dist));
-            GetComponent<Rigidbody2D>().MovePosition(transform.position + p);
+            mousePos = Input.GetTouch(0).position;
+            //mousePos = Input.GetTouch(0).position;
+            p = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 15));
+            transform.position = p;
+
         }
-        if (Input.touchCount > 1)
-        {
-            Debug.Log("bomb");
-        }
+    }
+    void OnGUI()
+    {
     }
 }
