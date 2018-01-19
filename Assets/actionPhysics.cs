b6695caaaaeb38575a80fd4077884e7223377fd1
sorlinV <valentin.sorlin@hotmail.com>
2018-01-19 15:44:37 +0100

    drone think

--- Assets/actionPhysics.cs
+++ Assets/actionPhysics.cs
@@ -8,8 +8,8 @@ public class actionPhysics : MonoBehaviour
     public GameObject bombPrebab;
     Vector2 oldPosition;
     Vector2 directionBomb;
-   
-	private void FixedUpdate()
+
+    private void FixedUpdate()
     {
         Vector2 newPosition = new Vector2(transform.position.x, transform.position.y);
         directionBomb = newPosition - oldPosition;
@@ -31,18 +31,15 @@ public class actionPhysics : MonoBehaviour
         while (i++ < nb_bullet)
         {
             float angle = ((i / nb_bullet) * angle_bullet) - (angle_bullet / 2);
-            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation * Quaternion.Euler(0, 0, angle)) as GameObject;
-            bullet.name = bulletPrefab.name;
-            Destroy(bullet, 2f);
+            foreach (Transform bulletSpawn in transform)
+            {
+                if(bulletSpawn.gameObject.tag == "bulletSpawn")
+                {
+                    GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation * Quaternion.Euler(0, 0, angle)) as GameObject;
+                    bullet.name = bulletPrefab.name;
+                    Destroy(bullet, 3f);
+                }
+            }
         }
     }
-
-/*	IEnumerator Start ()
-	{
-		while (true) {
-				Instantiate (bulletPrefab, new Vector3(transform.position.x + 0.2f, transform.position.y + 0.4f, transform.position.z), transform.rotation);
-				Instantiate (bulletPrefab, new Vector3(transform.position.x - 0.2f, transform.position.y + 0.4f, transform.position.z), transform.rotation);
-				yield return new WaitForSeconds (0.05f);
-			}
-	}*/
 }
