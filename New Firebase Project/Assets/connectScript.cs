b5f128595611f3d22acb31c77a132374d3d228d1
sorlinV <valentin.sorlin@hotmail.com>
2018-01-19 08:52:39 +0100

    google connect firebase

--- New Firebase Project/Assets/connectScript.cs
+++ New Firebase Project/Assets/connectScript.cs
@@ -75,5 +75,20 @@ public class connectScript : MonoBehaviour {
             Debug.LogFormat("User signed in successfully: {0} ({1})",
                 newUser.DisplayName, newUser.UserId);
         });
+        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+        if (user != null)
+        {
+            string name = user.DisplayName;
+            string email = user.Email;
+            System.Uri photo_url = user.PhotoUrl;
+            // The user's Id, unique to the Firebase project.
+            // Do NOT use this value to authenticate with your backend server, if you
+            // have one; use User.TokenAsync() instead.
+            string uid = user.UserId;
+            error.text = "name : " + name + "\n" +
+                "email : " + email + "\n" +
+                "photo_url : " + photo_url + "\n" +
+                "uid : " + uid;
+        }
     }
 }
