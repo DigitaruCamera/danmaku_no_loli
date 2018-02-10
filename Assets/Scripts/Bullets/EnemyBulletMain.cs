735d37795c136688ec9b2ffd1f05887968993992
sorlinV <valentin.sorlin@hotmail.com>
2018-02-10 22:14:00 +0100

    loadscript del useless

---
+++ Assets/Scripts/Bullets/EnemyBulletMain.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletMain : MonoBehaviour {
+
+    /* spawner avec la bullet avec le rotation corriger et a la hauteur du joueur
+       partir en direction du joueur
+       suivre le joueur
+       pouvoir faire spawn d'autre bullet (suivant plusieur condition)
+       suivre une fonction type f(x)
+       tu vois vois les boss fait des zone, puis y a des meteorite qui tombe dedans et ba pareil
+       mode railgun lazer
+
+
+      
+        pourvoir controller la vitesse/taille/etc suivant condition ou curve
+        https://docs.unity3d.com/ScriptReference/EditorGUI.CurveField.html
+       */
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/GamePlay/SceneManagerMainScript.cs
+++ Assets/Scripts/GamePlay/SceneManagerMainScript.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneManagerMainScript : MonoBehaviour {
+public class SceneManagerMainScript : MonoBehaviour
+{
+    public GameObject loadingImage;
 
-	void Start () {
+    void Start () {
 		Time.timeScale = 1;
 	}
-		
+
+    public void LoadScene(string SceneName)
+    {
+        loadingImage.SetActive(true);
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void LoadSameScene()
+    {
+        loadingImage.SetActive(true);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
     public void LoadSceneCredit(string SceneName)
     {
+        loadingImage.SetActive(true);
         SceneManager.LoadScene(SceneName);
         if (GetComponent<Ads>() != null)
         {
@@ -20,6 +35,7 @@ public class SceneManagerMainScript : MonoBehaviour {
 
     public void LoadSameSceneCredit()
     {
+        loadingImage.SetActive(true);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         if (GetComponent<Ads>() != null)
         {
@@ -29,6 +45,7 @@ public class SceneManagerMainScript : MonoBehaviour {
 
     public void ContinueCredit()
     {
+        loadingImage.SetActive(true);
         if (FindObjectOfType<Player>() != null)
         {
             FindObjectOfType<Player>().deathDisableCredit();
