30d981f854e376017be47110f8c467004a92f921
sorlinV <valentin.sorlin@hotmail.com>
2018-03-19 19:00:34 +0100

    adding gestion DontDestroyOnLoad

---
+++ Assets/Scripts/DontDestroyOnload.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DontDestroyOnload : MonoBehaviour {
+	void Start () {
+        foreach (GameObject go in FindObjectsOfType<GameObject>())
+        {
+            DontDestroyOnLoad(go);
+        }
+        Destroy(GetComponent<DontDestroyOnload>());
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
